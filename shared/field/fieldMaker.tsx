07a641cfdc421ba72f009f96ef4a8988d5dcc50f
sahil-kikani <sahilkikani111@gmail.com>
2023-12-19 15:48:49 +0530

    change

--- shared/field/fieldMaker.tsx
+++ shared/field/fieldMaker.tsx
@@ -3,12 +3,17 @@ import CommonInput from '../ui/commonInput'
 import Divider from '../ui/divider'
 import CustomImage from '../ui/customImage'
 import FieldBottom from './fieldBottom'
-import { Combobox, Listbox, RadioGroup, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
 import Dropdown from '../ui/dropdown'
 import RadioButton from '../ui/radioButton'
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
 
-export default function FieldMaker() {
+type FieldMakerTypes = {
+  field: any
+}
+
+export default function FieldMaker({ field }: FieldMakerTypes) {
   const {
     control,
     handleSubmit,
@@ -16,82 +21,46 @@ export default function FieldMaker() {
     formState: { errors }
   } = useForm()
 
-  const options = [{ name: 'Option 1' }, { name: 'Option 2' }, { name: 'Option 3' }]
+  const { attributes, setNodeRef,listeners, transform, transition } = useSortable({
+    id: field.id
+  })
 
-  const [selectedoption, setSelectedoption] = useState(options[0])
+  const style = {
+     transform: CSS.Transform.toString(transform),
+     transition
+  }
 
   return (
     <>
-      <div className='h-[186px] border-2 rounded-lg m-4 cursor-pointer hover:border-primary-500'>
-        <div className='h-[135px] p-2 relative'>
-          <CommonInput className='mt-4' disabled label='Label' type='audio' register={register} name='sTitle' placeholder='first name' />
-          <div className='absolute top-0 left-0 h-full w-full'></div>
-        </div>
-        <Divider />
-        <FieldBottom />
-      </div>
-      <div className='h-[186px] border-2 rounded-lg m-4 cursor-pointer hover:border-primary-500 '>
-        <div className='h-[135px] p-2 relative'>
-          <CommonInput className='mt-4 ' disabled label='Label' type='textarea' register={register} name='sTitle' placeholder='text area' />
-          <div className='absolute top-0 left-0 h-full w-full'></div>
-        </div>
-        <Divider />
-        <FieldBottom />
-      </div>
-      <div className='h-[186px] border-2 rounded-lg m-4 cursor-pointer hover:border-primary-500'>
-        <div className='h-[135px] p-2 relative'>
-          <CommonInput
-            className='mt-4 '
-            disabled
-            label='Number'
-            type='number'
-            register={register}
-            name='sTitle'
-            placeholder='Enter Number'
-          />
-          <div className='absolute top-0 left-0 h-full w-full'></div>
-        </div>
-        <Divider />
-        <FieldBottom />
-      </div>
-      <div className='h-[186px] border-2 rounded-lg m-4 cursor-pointer hover:border-primary-500'>
-        <div className='h-[135px] p-2 relative'>
-          <CommonInput
-            className='mt-4'
-            disabled
-            label='Email Address'
-            type='email'
-            register={register}
-            name='sEmail'
-            placeholder='Enter Number'
-          />
-          <div className='absolute top-0 left-0 h-full w-full'></div>
-        </div>
-        <Divider />
-        <FieldBottom />
-      </div>
-      <div className='h-[186px] border-2 rounded-lg m-4 cursor-pointer hover:border-primary-500'>
-        <div className='h-[135px] p-2 relative flex justify-left items-center'>
-          <label>Heading Text</label>
-          <div className='absolute top-0 left-0 h-full w-full'></div>
-        </div>
-        <Divider />
-        <FieldBottom />
-      </div>
-      <div className='h-[186px] border-2 rounded-lg m-4 cursor-pointer hover:border-primary-500'>
+      <div className='h-[186px] border-2 rounded-lg m-4 hover:border-primary-500 cursor-move' style={style} ref={setNodeRef} {...attributes} {...listeners}>
         <div className='h-[135px] p-2 relative '>
-          <label>Dropdown</label>
-          <Dropdown options={options} className='mt-2 ' />
-          <div className='absolute top-0 left-0 h-full w-full'></div>
-        </div>
-        <Divider />
-        <FieldBottom />
-      </div>
-
-      <div className='h-[186px] border-2 rounded-lg m-4 cursor-pointer hover:border-primary-500'>
-        <div className='h-[135px] p-2 relative '>
-          <RadioButton options={options} label='label' />
-          <div className='absolute top-0 left-0 h-full w-full'></div>
+          {field.type === 'select' && (
+            <>
+              <label>{field?.oSettings?.label}</label>
+              <Dropdown options={field?.oSettings?.options} className='mt-2 ' />
+              <div className='absolute top-0 left-0 h-full w-full cursor-move'></div>
+            </>
+          )}
+          {field.subType === 'radio' && (
+            <>
+              <RadioButton options={field?.oSettings?.options} label={field?.oSettings?.label} />
+              <div className='absolute top-0 left-0 h-full w-full cursor-move'></div>
+            </>
+          )}
+          {(field.type === 'input' || field.type === 'textarea') && field?.subType !== 'radio' && field && (
+            <>
+              <CommonInput
+                className='mt-4 '
+                disabled
+                label={field.oSettings.label}
+                type={field.type === 'input' ? field?.subType : field?.type}
+                register={register}
+                name='sTitle'
+                placeholder={field.oSettings.placeholder}
+              />
+              <div className='absolute top-0 left-0 h-full w-full cursor-move'></div>
+            </>
+          )}
         </div>
         <Divider />
         <FieldBottom />
